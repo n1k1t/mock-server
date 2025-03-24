3d60cb5d7d33a9715e1780d136bd7956c9e9cd9b
n1k1t <n1k1t.developer@gmail.com>
2025-03-24 21:53:41 +0300

    fix: dev script

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,9 @@
+import type { Configuration } from 'webpack';
+
 import LiveReloadPlugin from 'webpack-livereload-plugin';
 import path from 'path';
 import fs from 'fs/promises';
 
-import { Configuration } from 'webpack';
-
-import { TFunction } from './src/types';
 import { cast } from './src/utils';
 
 class LiveReloadWebpackPlugin extends LiveReloadPlugin {
@@ -13,84 +12,78 @@ class LiveReloadWebpackPlugin extends LiveReloadPlugin {
   }
 }
 
-export default cast<TFunction<Configuration[], [object, Pick<Configuration, 'mode'>]>>((env, options) => {
-  if (options.mode === 'development') {
-    require('./test');
-  }
+export default cast<Configuration[]>([
+  {
+    name: 'app',
 
-  return [
-    {
-      name: 'app',
-
-      entry: path.join(__dirname, 'src', 'gui', 'app', 'main.ts'),
-      output: {
-        filename: 'app.js',
-        path: path.join(__dirname, 'public', 'scripts'),
-      },
-
-      module: {
-        rules: [{
-          test: /\.ts$/i,
-          loader: 'ts-loader',
-
-          options: {
-            configFile: path.join(__dirname, 'src', 'gui', 'app', 'tsconfig.json'),
-          },
-        }],
-      },
-
-      resolve: {
-        extensions: ['.js', '.ts', '.json', '.hbs'],
-
-        fallback: {
-          fs: false,
-        },
-      },
-
-      plugins: [new LiveReloadWebpackPlugin()],
+    entry: path.join(__dirname, 'src', 'gui', 'app', 'main.ts'),
+    output: {
+      filename: 'app.js',
+      path: path.join(__dirname, 'public', 'scripts'),
     },
-    {
-      name: 'styles',
 
-      entry: path.join(__dirname, 'src', 'gui', 'styles', 'main.scss'),
-      output: {
-        filename: 'styles.js',
-        path: path.join(__dirname, 'public', 'scripts'),
+    module: {
+      rules: [{
+        test: /\.ts$/i,
+        loader: 'ts-loader',
+
+        options: {
+          configFile: path.join(__dirname, 'src', 'gui', 'app', 'tsconfig.json'),
+        },
+      }],
+    },
+
+    resolve: {
+      extensions: ['.js', '.ts', '.json', '.hbs'],
+
+      fallback: {
+        fs: false,
       },
+    },
 
-      module: {
-        rules: [{
-          test: /\.(sa|sc|c)ss$/i,
-          use: [
-            'style-loader',
-            'css-loader',
-            {
-              loader: 'sass-loader',
-              options: {
-                additionalData: async (content: string, additional: { context: string }) => {
-                  const lines = content.toString().split('\n');
-                  const imports = lines
-                    .map((line, index) => <const>[line, index])
-                    .filter(([line]) => line.includes('@import') && line.includes('*'))
-                    .map(([line, index]) => <const>[line.trim().replace(/\@import\s'|';/g, ''), index]);
+    plugins: [new LiveReloadWebpackPlugin()],
+  },
+  {
+    name: 'styles',
 
-                  for (const [minimatch, index] of imports.reverse()) {
-                    lines.splice(index, 1);
+    entry: path.join(__dirname, 'src', 'gui', 'styles', 'main.scss'),
+    output: {
+      filename: 'styles.js',
+      path: path.join(__dirname, 'public', 'scripts'),
+    },
 
-                    for await (const target of fs.glob(path.resolve(additional.context, minimatch))) {
-                      lines.push(`@import '${target}';`);
-                    }
+    module: {
+      rules: [{
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              additionalData: async (content: string, additional: { context: string }) => {
+                const lines = content.toString().split('\n');
+                const imports = lines
+                  .map((line, index) => <const>[line, index])
+                  .filter(([line]) => line.includes('@import') && line.includes('*'))
+                  .map(([line, index]) => <const>[line.trim().replace(/\@import\s'|';/g, ''), index]);
+
+                for (const [minimatch, index] of imports.reverse()) {
+                  lines.splice(index, 1);
+
+                  for await (const target of fs.glob(path.resolve(additional.context, minimatch))) {
+                    lines.push(`@import '${target}';`);
                   }
+                }
 
-                  return lines.join('\n');
-                },
+                return lines.join('\n');
               },
             },
-          ],
-        }],
-      },
+          },
+        ],
+      }],
+    },
 
-      plugins: [new LiveReloadWebpackPlugin()],
-    }
-  ];
-});
+    plugins: [new LiveReloadWebpackPlugin()],
+  },
+]);
