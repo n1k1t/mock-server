c81acdf19b6b97e90a68166edd2e6054a6d186e2
n1k1t <n1k1t.developer@gmail.com>
2025-06-05 14:06:30 +0300

    fix: .npmignore & types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 export { default as config } from './config';
 
 export * from './expectations/types';
+export * from '../types';
+
 export * from './server';
 export * from './client';
 export * from './logger';
-export * from '../types';
 export * from './meta';
